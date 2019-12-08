75d44a0edd68201c008f572098172df7f77c1b14
Aymane <danielayman@gmail.com>
2019-12-09 00:29:47 +0100

    put and delete

--- app.js
+++ app.js
@@ -34,7 +34,17 @@ app.post('/api/products', (req, res, next) => {
             res.status(400).json({ error })}
             );
 });
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Modified!'}))
+        .catch(error => res.status(400).json({ error }));
+});
 
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Deleted!'}))
+        .catch(error => res.status(400).json({ error }));
+});
 app.get('/api/products/:id', (req, res, next) => {
     Product.findOne({ _id: req.params.id })
         .then(product => res.status(200).json({ product: product }))
